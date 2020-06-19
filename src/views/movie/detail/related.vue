a1bb516dc56433f5f6727eb660d451f100d1bbb0
guomingyang <myguo@aibee.com>
2020-06-19 09:45:20 +0800

    feat: related movies (Doing)

--- src/views/movie/detail/related.vue
+++ src/views/movie/detail/related.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="related"></div>
+  <div id="related">
+    <div class="related-title">Related Titles</div>
+    <el-divider></el-divider>
+    <div class="related-items"></div>
+  </div>
 </template>
 
 <script>
+  import { defaultMovieDataMixins } from '@/views/movie/mixins/common'
+
   export default {
-    name: 'Related'
+    name: 'Related',
+    mixins: [defaultMovieDataMixins]
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  @import "~@/style/movie/detail/related.scss";
 </style>
