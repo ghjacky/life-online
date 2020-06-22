3406863bffb10a792c14d2b7a2182b57b1e70b0b
guomingyang <myguo@aibee.com>
2020-06-22 10:04:43 +0800

    feat: related item component (doing)

--- src/views/movie/detail/related.vue
+++ src/views/movie/detail/related.vue
@@ -2,16 +2,27 @@
   <div id="related">
     <div class="related-title">Related Titles</div>
     <el-divider></el-divider>
-    <div class="related-items"></div>
+    <div class="related-items">
+      <relatem v-for="(item,index) in relatedItems" :key="index" :item="item"></relatem>
+    </div>
   </div>
 </template>
 
 <script>
   import { defaultMovieDataMixins } from '@/views/movie/mixins/common'
+  import Relatem from '@/views/movie/detail/relatem'
 
   export default {
     name: 'Related',
-    mixins: [defaultMovieDataMixins]
+    components: { Relatem },
+    mixins: [defaultMovieDataMixins],
+    data() {
+      return {
+        relatedItems: [
+          defaultMovieDataMixins
+        ]
+      }
+    }
   }
 </script>
 
---
+++ src/views/movie/detail/relatem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="related-item">
+    <div class="cover">
+      <el-image></el-image>
+    </div>
+    <div class="stuff">
+      <div class="title"></div>
+      <div class="publish-duration"></div>
+      <div class="tag"></div>
+    </div>
+    <div class="rate"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Relatem',
+    props: {
+      item: {
+        type: Object,
+        default: () => {
+          return {
+            id: 0,
+            title: '',
+            summary: '',
+            category: '',
+            tag: '',
+            publish: '',
+            duration: '',
+            rating: '',
+            director: '',
+            starring: '',
+            cover: '',
+            poster: ''
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
