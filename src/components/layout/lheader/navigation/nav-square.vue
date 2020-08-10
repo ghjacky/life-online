73c6aaf38fcb836821b10260cea6bddccaf09d6f
guomingyang <myguo@aibee.com>
2020-08-10 09:56:16 +0800

    feat: add nav square element

--- src/components/layout/lheader/navigation/nav-square.vue
+++ src/components/layout/lheader/navigation/nav-square.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="nav-square" v-if="isShow"></div>
+  <div id="nav-square" v-if="isShow">
+    <div v-for="(nav, index) in navs" :key="index" class="nav-item">
+      <div class="nav-dimension">{{ nav.Dimension.label }}</div>
+      <div class="nav-list">
+        <ul>
+          <li v-for="(l,i) in nav.list" :key="i">{{ l }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +18,14 @@
       isShow: {
         type: Boolean,
         default: false
+      }
+    },
+    data() {
+      return {
+        navs: [
+          { Dimension: { label: '分类', prop: 'category' }, list: ['1', '2', '3'] },
+          { Dimension: { label: '分类', prop: 'category' }, list: ['1', '2', '3'] }
+        ]
       }
     }
   }
